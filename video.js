a1fecaeafa0128287814e0d2e0da6991f46c0eff
unknown <davidalpalacio@gmail.com>
2022-04-20 21:22:01 -0500

    myfirst_coment

---
+++ video.js
@@ -0,0 +1,8 @@
+
+      const lightbox = GLightbox({
+        'href': 'https://youtu.be/iJKCj8uAHz8',
+            'type': 'video',
+            'source': 'youtube', //vimeo, youtube or local
+            'width': 900,
+            'autoPlayVideos':'true',
+        });
